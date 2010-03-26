f6013a76bf90b644b1b215065c190fc5286c8f3e
Aurelien Gateau <agateau@kde.org>
2010-03-26 12:40:19 +0100

    Improved parser handling

--- cmdline.py
+++ cmdline.py
@@ -2,9 +2,10 @@
 import sys
 from optparse import OptionParser
 
+USAGE="%prog <arg1> <arg2>..."
 
 def main():
-    parser = OptionParser()
+    parser = OptionParser(usage=USAGE)
 
     # Add an option which takes an argument and is stored in options.filename.
     # 'metavar' is an example of argument and should match the text in 'help'.
@@ -12,6 +13,11 @@ def main():
                       help="write report to FILE", metavar="FILE")
 
     # Add a boolean option stored in options.verbose.
+    parser.add_option("-x", "--xyz",
+                      action="store_true", dest="xyz", default=True,
+                      help="use 'xyz' method")
+
+    # Add an invert boolean option stored in options.verbose.
     parser.add_option("-q", "--quiet",
                       action="store_false", dest="verbose", default=True,
                       help="don't print status messages to stdout")
@@ -21,6 +27,9 @@ def main():
     print "filename:", options.filename
     print "verbose:", options.verbose
     print "args:", args
+
+    if len(args) == 0:
+        parser.error("Missing args")
 
     return 0
 
