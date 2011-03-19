9422c757fa47cfe18819790b8c595502f668b683
Aurélien Gâteau <agateau@kde.org>
2011-03-19 10:52:08 +0100

    Revert main.cpp change

--- qt4/main.cpp
+++ qt4/main.cpp
@@ -1,29 +1,10 @@
-cmake_minimum_required(VERSION 2.8)
+#include <QtGui/QApplication>
+#include <QtGui/QWidget>
 
-# Dependencies
-find_package(Qt4 REQUIRED)
 
-# Sources
-set(template_SRCS
-    main.cpp
-    )
-
-# Building
-include_directories(
-    ${QT_INCLUDE_DIR}
-    ${QT_QTCORE_INCLUDE_DIR}
-    ${QT_QTGUI_INCLUDE_DIR}
-    )
-
-qt4_automoc(${template_SRCS})
-add_executable(template ${template_SRCS})
-
-target_link_libraries(template
-    ${QT_QTGUI_LIBRARIES}
-    ${QT_QTCORE_LIBRARIES}
-    )
-
-# Install
-install(TARGETS template
-    RUNTIME DESTINATION bin
-    )
+int main(int argc, char** argv) {
+	QApplication app(argc, argv);
+	QWidget* widget = new QWidget();
+	widget->show();
+	return app.exec();
+}
