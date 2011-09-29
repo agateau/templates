0a002aaa34340e9b6ea5ba26760728f86d13eb2a
deveba <deveba@trinity>
2011-09-29 17:10:31 +0200

    Automatic commit from deveba, running on trinity (group daily)

--- qt4/main.cpp
+++ qt4/main.cpp
@@ -1,10 +1,10 @@
-#include <QtGui/QApplication>
-#include <QtGui/QWidget>
+#include <QApplication>
+#include <QWidget>
 
-
-int main(int argc, char** argv) {
-	QApplication app(argc, argv);
-	QWidget* widget = new QWidget();
-	widget->show();
-	return app.exec();
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    QWidget* widget = new QWidget();
+    widget->show();
+    return app.exec();
 }
