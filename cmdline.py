fa646697516bf44cae9fca3fb91be5ea4a71ca31
Aurélien Gâteau <agateau@kde.org>
2013-02-07 11:18:12 +0100

    Migrate to argparse

--- cmdline.py
+++ cmdline.py
@@ -1,36 +1,32 @@
 #!/usr/bin/env python
 # encoding: utf-8
 import sys
-from optparse import OptionParser
+import argparse
 
-USAGE = "%prog <arg1> <arg2>..."
+DESCRIPTION = """\
+"""
 
 def main():
-    parser = OptionParser(usage=USAGE)
+    parser = argparse.ArgumentParser(description=DESCRIPTION)
 
-    # Add an option which takes an argument and is stored in options.filename.
-    # 'metavar' is an example of argument and should match the text in 'help'.
-    parser.add_option("-f", "--file", dest="filename",
-                      help="write report to FILE", metavar="FILE")
+    parser.add_argument("-f", "--file", dest="filename",
+        help="write report to FILE", metavar="FILE")
 
-    # Add a boolean option stored in options.verbose.
-    parser.add_option("-x", "--xyz",
-                      action="store_true", dest="xyz", default=True,
-                      help="use 'xyz' method")
+    parser.add_argument("-n", "--dry-run", action="store_true",
+        help="Simulate")
 
-    # Add an invert boolean option stored in options.verbose.
-    parser.add_option("-q", "--quiet",
-                      action="store_false", dest="verbose", default=True,
-                      help="don't print status messages to stdout")
+    parser.add_argument("-v", "--verbose", action="count",
+        help="Be more verbose")
 
-    (options, args) = parser.parse_args()
+    parser.add_argument("mandatory_arg", nargs=1)
+    parser.add_argument("optional_arg", nargs="?")
 
-    print "filename:", options.filename
-    print "verbose:", options.verbose
-    print "args:", args
+    args = parser.parse_args()
 
-    if len(args) == 0:
-        parser.error("Missing args")
+    print "filename:", args.filename
+    print "verbose:", args.verbose
+    print "mandatory_arg:", args.mandatory_arg
+    print "optional_arg:", args.optional_arg
 
     return 0
 
