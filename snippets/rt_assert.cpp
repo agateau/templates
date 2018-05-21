f705b8868f51774f36072902fe2cf8a8586ee113
deveba <deveba@dozer>
2018-05-21 11:59:12 +0200

    Automatic commit from deveba, running on dozer (group hourly)

--- snippets/rt_assert.cpp
+++ snippets/rt_assert.cpp
@@ -1,8 +1,11 @@
-#include "rtassert.h"
+#include "rt_assert.h"
 
+void my_func() {
+    RT_ASSERT(1 > 2);
+}
 
 int main(int argc, char** argv) {
     RT_ASSERT(argc > 1, "Not enough arguments");
-    RT_ASSERT(1 > 2);
+    my_func();
     return 0;
 }
