05f354ba89caa2d0a2ed9515c86728f186f1daf7
deveba <deveba@dozer>
2018-11-29 17:30:22 +0100

    Automatic commit from deveba, running on dozer (group hourly)

--- snippets/interpreter.py
+++ snippets/interpreter.py
@@ -1,7 +1,7 @@
 from code import InteractiveConsole
 
 
-# Copy this where to get an interactive console running with access to the
-# local variables
+# Copy this where you want to get an interactive console running with access to
+# the local variables
 csl = InteractiveConsole(locals=locals())
 csl.interact()
